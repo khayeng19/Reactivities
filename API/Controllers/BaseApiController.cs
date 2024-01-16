c1a669818a9758d7ff95aa3c2e4b1bfc91754dc5
Angelo <manalac.angelo23@yahoo.com>
2024-01-16 18:14:09 +0800

    New

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // localhost:5000/weatherforcast
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
